4e0827f2ca66b70273d7ce4459e6f33af183b439
Mohammed Ibrahim <163002011+Mohamediibra7im@users.noreply.github.com>
2025-07-24 19:44:17 +0300

    Refactor ModeToggle to remove automatic theme setting and improve NavBar button formatting

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect } from "react";
 import * as React from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
@@ -15,10 +14,6 @@ import {
 
 const ModeToggle = () => {
   const { setTheme } = useTheme();
-
-  useEffect(() => {
-    setTheme("system");
-  }, [setTheme]);
 
   return (
     <DropdownMenu>
